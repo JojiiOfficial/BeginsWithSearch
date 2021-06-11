06fd675369c0bfe27f600b9d4d4c79537c4e68aa
jojii <jojii@gmx.net>
2021-06-11 16:04:32 +0200

    improve algo, add tests

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -3,7 +3,8 @@ use std::cmp::Ordering;
 use crate::store_item::Item;
 use crate::text_store::TextStore;
 
-/// A search object. Implements the serach algorithm
+/// A search object. Implements the binary search algorithm for substring matching. Implement
+/// `Iterator` which iterates over the results
 pub(crate) struct Search<'a, S: TextStore> {
     store: &'a S,
     query: &'a str,
@@ -13,6 +14,7 @@ pub(crate) struct Search<'a, S: TextStore> {
 }
 
 impl<'a, S: TextStore> Search<'a, S> {
+    /// Create a new search item. Allows to be used as iterator over results
     pub(crate) fn new(store: &'a S, query: &'a str) -> Self {
         Self {
             store,
@@ -23,13 +25,21 @@ impl<'a, S: TextStore> Search<'a, S> {
         }
     }
 
+    /// Checks if the search input is valid
     fn is_valid(&self) -> bool {
         let store_len = self.store.len();
         let query = self.query;
         store_len != 0 && !query.is_empty()
     }
 
-    fn compare(&self, i: &str) -> Ordering {
+    /// Compares the item with the query
+    fn compare(&self, i: &S::Item) -> Ordering {
+        let i = i.get_text();
+
+        if self.query.len() > i.len() {
+            //return Ordering::Less;
+        }
+
         let item_char = i
             .chars()
             .nth(self.last_str_index)
@@ -47,34 +57,70 @@ impl<'a, S: TextStore> Iterator for Search<'a, S> {
         self.is_valid().then(|| 0)?;
 
         loop {
-            if self.last_pos == 0 {
+            // End  reached
+            if self.last_pos == 0 && !self.found_first {
                 break None;
             }
 
             let curr_index = if self.found_first {
+                // Item found, we only want to iterate from here on
                 self.last_pos + 1
             } else {
+                // Continue binary search
                 self.last_pos / 2
             };
 
             let curr_item = self.store.get_at(curr_index)?;
 
-            match self.compare(curr_item.get_text()) {
+            match self.compare(curr_item) {
                 Ordering::Equal => {
-                    self.last_pos = curr_index;
-                    self.found_first = true;
+                    // We want to be able to match the entire substring, not only the first
+                    // character
+                    if self.last_str_index < self.query.len() - 1 {
+                        self.last_str_index += 1;
+                        continue;
+                    }
+
+                    let mut curr_item = curr_item;
+
+                    if !self.found_first {
+                        self.found_first = true;
+                        self.last_pos = curr_index;
+
+                        // Find first position
+                        for i in (0..curr_index).rev() {
+                            let citem = self.store.get_at(i)?;
+                            if self.compare(citem) == Ordering::Equal {
+                                curr_item = citem;
+                                self.last_pos = i;
+                            } else {
+                                break;
+                            }
+                        }
+                    } else {
+                        self.last_pos = curr_index;
+                    }
+
+                    // Skip entries with shorter text as query
                     if curr_item.get_text().len() < self.query.len() {
                         continue;
                     }
+
+                    // Item found
                     return Some(curr_item);
                 }
                 Ordering::Greater => {
+                    // Jump to bigger half
                     self.last_pos = curr_index + self.last_pos;
                 }
                 Ordering::Less => {
+                    // Items already found. This indicates the end of all matching items since the
+                    // input is sorted
                     if self.found_first {
                         return None;
                     }
+
+                    // Jump to lower half
                     self.last_pos = curr_index;
                 }
             }
--- src/lib.rs
+++ src/lib.rs
@@ -48,12 +48,47 @@ mod tests {
         TextSearch::new(vec!["a", "abc", "add", "b", "bbc"])
     }
 
-    fn bigger_ts() -> TextSearch<Vec<&'static str>> {
-        let input = vec!["a", "b", "go", "golang", "rust"];
-        let mut input_sorted = vec!["a", "b", "go", "golang", "rust"];
-        input_sorted.sort_unstable();
-        assert_eq!(input, input_sorted);
-        TextSearch::new(input_sorted)
+    fn simple_ts2() -> TextSearch<Vec<&'static str>> {
+        TextSearch::new(vec!["b", "bbc"])
+    }
+
+    fn simple_dataset() -> Vec<TextSearch<Vec<&'static str>>> {
+        vec![simple_ts(), simple_ts2()]
+        //vec![simple_ts2()]
+    }
+
+    #[test]
+    fn first_matches() {
+        for search in simple_dataset() {
+            let e = search.find_all("b");
+            assert_eq!(e, vec![&"b", &"bbc"]);
+        }
+    }
+
+    #[test]
+    fn empty_query() {
+        for search in simple_dataset() {
+            let e = search.find_all("");
+            let empty: Vec<&&str> = Vec::new();
+            assert_eq!(e, empty);
+        }
+    }
+
+    #[test]
+    fn not_found() {
+        for search in simple_dataset() {
+            let e = search.find_all("0");
+            let empty: Vec<&&str> = Vec::new();
+            assert_eq!(e, empty);
+        }
+    }
+
+    #[test]
+    fn test_2st_matches() {
+        for search in simple_dataset() {
+            let e = search.find_all("bb");
+            assert_eq!(e, vec![&"bbc"]);
+        }
     }
 
     #[test]
@@ -72,39 +107,25 @@ mod tests {
     }
 
     #[test]
-    fn first_matches() {
-        let search = simple_ts();
-        let e = search.find_all("b");
-        assert_eq!(e, vec![&"b", &"bbc"]);
-    }
-
-    #[test]
-    fn empty_query() {
-        let search = simple_ts();
-        let e = search.find_all("");
-        let empty: Vec<&&str> = Vec::new();
-        assert_eq!(e, empty);
-    }
-
-    #[test]
-    fn not_found() {
-        let search = simple_ts();
-        let e = search.find_all("0");
-        let empty: Vec<&&str> = Vec::new();
-        assert_eq!(e, empty);
-    }
-
-    #[test]
-    fn test_2st_matches() {
-        let search = simple_ts();
-        let e = search.find_all("bb");
-        assert_eq!(e, vec![&"bbc"]);
-    }
-
-    #[test]
     fn test_matches_complex() {
         let search = bigger_ts();
         let e = search.find_all("go");
         assert_eq!(e, vec![&"go", &"golang"]);
+    }
+
+    #[test]
+    fn test_matches_complex2() {
+        let search = bigger_ts();
+        let e = search.find_all("ga");
+        let empty: Vec<&&str> = Vec::new();
+        assert_eq!(e, empty);
+    }
+
+    fn bigger_ts() -> TextSearch<Vec<&'static str>> {
+        let input = vec!["a", "b", "go", "golang", "rust"];
+        let mut input_sorted = vec!["a", "b", "go", "golang", "rust"];
+        input_sorted.sort_unstable();
+        assert_eq!(input, input_sorted);
+        TextSearch::new(input_sorted)
     }
 }
